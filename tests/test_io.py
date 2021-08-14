0eb5583612f9f54258d695b85e30935944554dfe
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-14 11:21:10 +0200

    (Test) parametrize test_integer__expected_input
    Why: Allow for the same test harness to be used for multiple
         tests.
    
    How: pytest.mark.parametrize
    
    Tags: test test_integer__expected_input

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,17 +1,37 @@
 import io
+import pytest
 from algos.io import ReadStdIn
 
 
+data__integer__expected_input = [
+    ("1", 1),
+    ("-1", -1),
+    ("0", 0),
+    ("1000000000000000000000", 1000000000000000000000)
+]
+"""
+Test cases for :class:`.ReadStdIn`, testing that it functions correctly for expected inputs.
+"""
+
+
 class TestReadStdIn:
-    def test_integer__expected_input(self, monkeypatch):
+    """
+    Test cases for :class:`.ReadStdIn`.
+    """
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        data__integer__expected_input,
+        ids=[repr(v) for v in data__integer__expected_input]
+    )
+    def test_integer__expected_input(self, monkeypatch, test_input, expected):
         """
         Test that the integer method works properly for expected inputs.
         """
         # Monkeypatch stdin to hold the value we want the program to read as input
-        monkeypatch.setattr('sys.stdin', io.StringIO("1"))
+        monkeypatch.setattr('sys.stdin', io.StringIO(test_input))
 
         # Create the instance
         reader = ReadStdIn()
 
         # Check that the value is the same as the monkeypatched value
-        assert reader.integer() == 1
+        assert reader.integer() == expected
