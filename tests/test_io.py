c278813fe0b45d72c01b524cb2246618c3cc0e2d
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-14 11:53:31 +0200

    (Document) update test_io docs
    Why: Correct cross references
    
    How: Change class reference to method reference
    
    Tags: documentation

--- tests/test_io.py
+++ tests/test_io.py
@@ -13,7 +13,7 @@ data__integer__expected_input = [
     ("1000000000000000000000", 1000000000000000000000)
 ]
 """
-Test cases for :class:`.ReadStdIn`, testing that it functions correctly for expected inputs.
+Test cases for :meth:`.ReadStdIn.integer`, testing that it functions correctly for expected inputs.
 """
 
 
@@ -28,7 +28,7 @@ class TestReadStdIn:
     )
     def test_integer__expected_input(self, monkeypatch, test_input, expected):
         """
-        Test that the integer method works properly for expected inputs.
+        Test that the :meth:`.ReadStdIn.integer` method works properly for expected inputs.
         """
         # Monkeypatch stdin to hold the value we want the program to read as input
         monkeypatch.setattr('sys.stdin', io.StringIO(test_input))
