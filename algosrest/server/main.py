3987726849384e7465303bd38e96aa36b3a10e2e
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-15 18:28:59 +0200

    (REST) add text anagrams endpoint
    Why: Service the anagrams algorithm from this endpoint.
    
    How: Create an app route for POST requests.
    
    Tags: REST text.anagrams

--- algosrest/server/main.py
+++ algosrest/server/main.py
@@ -3,9 +3,19 @@ Main module for REST server. Launch with
 
 .. code-block:: bash
 
-   (sudo) uvicorn backtester:app --reload --host 127.0.0.1 --port 8081
+   (sudo) uvicorn main:app --reload --host 127.0.0.1 --port 8081
 
 """
+import json
 from fastapi import FastAPI, Form, Body, File, Request
 from fastapi.responses import StreamingResponse
 
+app = FastAPI()
+"""The REST server instance itself"""
+
+
+@app.post("/text/anagrams")
+@app.post("/text/anagrams/")
+async def anagrams(json_data: Request):
+    body = json.loads(bytes(await json_data.body()).decode('utf8'))
+    print(body)
