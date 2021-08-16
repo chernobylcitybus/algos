3906a1af0890b980547b01c42ea6a00fe897be92
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-16 12:04:26 +0200

    (CI) exclude tests package from installation
    Why: Throwing off coverage by trying to run tests package.
    
    How: Explicitly name packages in setup.py
    
    Tags: continuous integration

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ setuptools.setup(
     version="0.1.0",
     author='Warren Jitsing',
     author_email='warren.jitsing@gmail.com',
-    packages=setuptools.find_packages(
-        where='.',
-        include=['*',],
-        exclude=[]
-    ),
+    packages=["algos", "algoscli", "algosrest"],
     entry_points={
         'console_scripts': [
             'algos-text=algoscli.main:text',
