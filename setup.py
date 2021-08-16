84a50f71be38bc960b42bfcc5b254f30ca47c129
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-16 12:25:48 +0200

    (CI) revert setup.py
    Why: Change back to automatic package discovery.
    
    How: Use setuptools.find_packages
    
    Tags: continous integration

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setuptools.setup(
     version="0.1.0",
     author='Warren Jitsing',
     author_email='warren.jitsing@gmail.com',
-    packages=["algos", "algoscli", "algosrest"],
+    packages=setuptools.find_packages(
+        where='.',
+        include=['*',],
+        exclude=[]
+    ),
     entry_points={
         'console_scripts': [
             'algos-text=algoscli.main:text',
