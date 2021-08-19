a3d6d76d72d972cd19602d03199ce72d705cd962
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-19 12:55:52 +0200

    (Test) add unit tests for Request object.
    Why: Check that it initializes correctly.
    
    How: Assert that everything was initialized properly.
    
    Tags: test algosrest.client.request

---
+++ tests/test_rest_client_request__unit.py
@@ -0,0 +1,24 @@
+from algosrest.client.parallel import RequestPool, ProcessPool
+from algosrest.client.text import TextRest
+from algosrest.client.request import Request
+
+
+class TestRequest:
+    """
+    Test that the container API works as expected.
+    """
+    def test_init(self):
+        # Create our request object.
+        req = Request(2, "localhost", 8081)
+
+        # Check that everything is of the correct class.
+        assert isinstance(req.req, RequestPool)
+        assert isinstance(req.req.pool, ProcessPool)
+        assert isinstance(req.text, TextRest)
+
+        # Check that input data was stored correctly.
+        assert req.req.hostname == "localhost"
+        assert req.req.port == 8081
+
+        # Shut down the pool.
+        req.req.shutdown()
