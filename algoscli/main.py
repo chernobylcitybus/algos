0d5cbc05f69ad451b851f1d42c328d17ac0e127a
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-20 09:30:51 +0200

    (Document) update main command line information (Update) handler handling
    Why: Document logic, components and usage. Get rid of handler if-else.
    
    How: Create a dictionary with the handlers as values and subcommand as
         keys.
    
    Tags: documentation cli

--- algoscli/main.py
+++ algoscli/main.py
@@ -1,3 +1,21 @@
+"""
+This module yields the command line entry points to the algorithms in :any:`algos` . Each function in this module
+corresponds to an equivalent command line grouping. The command line groupings thus far are
+
++--------------------------------+-------------------------------------------------------------------------------+
+| Name                           | purpose                                                                       |
++================================+===============================================================================+
+| :func:`.text`                  | Expose the text algorithms. Represents :code:`algos-text` command line group. |
++--------------------------------+-------------------------------------------------------------------------------+
+
+All algorithms read input data through ``stdin`` . A typical usage would be
+
+.. code-block:: bash
+
+   echo "the elbow on the arc is below the car" | algos-text anagrams
+   [['arc', 'car'], ['elbow', 'below']]
+
+"""
 import argparse
 import sys
 
@@ -6,6 +24,14 @@ from algoscli.common import parse_arguments
 
 
 def text():
+    """
+    The main entrypoint for text based algorithms. The currently supported algorithms are
+
+    +-----------------------------------------+
+    | :func:`algos.text.anagrams`             |
+    +-----------------------------------------+
+
+    """
     # Import the text component functions
     from algoscli.text import component_functions
 
@@ -17,7 +43,18 @@ def text():
         component_functions
     )
 
+    # Create a TextCLI instance for the subcommand handlers.
     text_instance: TextCLI = TextCLI()
 
-    if sys.argv[1] == "anagrams":
-        text_instance.anagrams()
+    # Create a dictionary so that we don't have a massive if-else statement to choose the subcommand handler.
+    handlers = {"anagrams": text_instance.anagrams}
+
+    # Get the appropriate handler, depending on sys.argv[1] .
+    handler = handlers.get(sys.argv[1], None)
+
+    # Handle the case of an unrecognized command.
+    if handler is None:
+        raise ValueError("Unknown subcommand " + sys.argv[1])
+
+    # Execute command line.
+    handler()
--- docs/source/cli.rst
+++ docs/source/cli.rst
@@ -1,6 +1,19 @@
 Command Line Interface
 ======================
 
+This package supports using the :any:`algos` package as a command line interface. The modules and their purposes are
+as follows
+
++--------------------------------------+---------------------------------------------------------------------------+
+| module                               | purpose                                                                   |
++======================================+===========================================================================+
+| :mod:`algoscli.common`               | Contains common functions for the CLI such as command line parsing.       |
++--------------------------------------+---------------------------------------------------------------------------+
+| :mod:`algoscli.main`                 | Contains the entry points for the console scripts.                        |
++--------------------------------------+---------------------------------------------------------------------------+
+| :mod:`algoscli.text`                 | Wrappers for the :mod:`algos.text` module to allow calling from shell.    |
++--------------------------------------+---------------------------------------------------------------------------+
+
 .. toctree::
 
    cli_common
